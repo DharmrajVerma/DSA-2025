8d7366d6938cc2afc95cb072c3a8ce87330368ae
DharmrajVerma <dharmrajverma184@gmail.com>
2025-08-16 22:45:57 +0530

    deepcopy

--- 10Stacks/2StackSTL.cpp
+++ 10Stacks/2StackSTL.cpp
@@ -1,21 +1,26 @@
-//Here we print the stack and at the last we have empty stack 
 #include<iostream>
 #include<stack>
 using namespace std;
 int main(){
     stack<int> st;
-    cout<<st.size()<<endl;
     st.push(10);
     st.push(20);
     st.push(30);
     st.push(40);
     st.push(50);
     st.push(60);
-    cout<<st.size()<<endl;
-
+    stack<int> temp;
     while(st.size()>0){
         cout<<st.top()<<" ";
+        int x= st.top();
         st.pop();
+        temp.push(x);
     }
-
+    cout<<endl<<st.size();
+    while(temp.size()>0){
+        int x=temp.top();
+        temp.pop();
+        st.push(x);
+    }
+    cout<<endl<<st.size();
 }
